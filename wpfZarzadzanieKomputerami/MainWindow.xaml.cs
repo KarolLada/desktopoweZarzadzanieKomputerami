bf4df5659436049f48e61d55964559b2643cc2d4
Kacper C <kacper.vandel@gmail.com>
2026-04-15 13:50:56 +0200

    Dodawanie użytkowników w zakładce użytkownicy

--- wpfZarzadzanieKomputerami/MainWindow.xaml.cs
+++ wpfZarzadzanieKomputerami/MainWindow.xaml.cs
@@ -1,24 +1,64 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace wpfZarzadzanieKomputerami
+namespace wpfZarzadzanieKomputer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<User> Users;
+
         public MainWindow()
         {
-            
+            InitializeComponent();
+
+            Users = new ObservableCollection<User>();
+            UsersGrid.ItemsSource = Users;
         }
+
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginBox.Text;
+            string password = PasswordBox.Password;
+
+            string accountType =
+                (AccountTypeBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "";
+
+            decimal balance = 0;
+            decimal.TryParse(BalanceBox.Text, out balance);
+
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                MessageBox.Show("Podaj login!");
+                return;
+            }
+
+            Users.Add(new User
+            {
+                Login = login,
+                Password = password,
+                AccountType = accountType,
+                Balance = balance
+            });
+
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            LoginBox.Text = "";
+            PasswordBox.Password = "";
+            AccountTypeBox.SelectedIndex = -1;
+            BalanceBox.Text = "";
+        }
+    }
+
+    // MODEL
+    public class User
+    {
+        public string Login { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string AccountType { get; set; } = "";
+        public decimal Balance { get; set; }
     }
 }
